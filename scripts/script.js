552d5b578c0e97678ca508da8998d505929311a1
a.m.korolev <a.m.korolev@tinkoff.ru>
2018-12-21 07:32:56 +0300

    Fixed sliders

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,83 @@
-var feedbackButton = document.querySelector('.feedback-button');
-var feedbacPopup = document.querySelector('.feedback.popup');
+(function () {
+  var feedbackButton = document.querySelector('.feedback-button');
+  var feedbacPopup = document.querySelector('.feedback.popup');
 
-var mapButton = document.querySelector('.contact__map');
-var mapPopup = document.querySelector('.full-map');
+  var mapButton = document.querySelector('.contact__map');
+  var mapPopup = document.querySelector('.full-map');
 
-var classNameOpenedPopup = '_open';
-var classNameCloseButton = 'popup__close';
+  var classNameOpenedPopup = '_open';
+  var classNameCloseButton = 'popup__close';
 
-mapButton.addEventListener('click', function() {
-  mapPopup.classList.add(classNameOpenedPopup);
-});
+  mapButton.addEventListener('click', function() {
+    mapPopup.classList.add(classNameOpenedPopup);
+  });
 
-feedbackButton.addEventListener('click', function() {
-  feedbacPopup.classList.add(classNameOpenedPopup);
-});
+  feedbackButton.addEventListener('click', function() {
+    feedbacPopup.classList.add(classNameOpenedPopup);
+  });
 
-document.addEventListener('click', function(event) {
-  var target = event.target;
+  document.addEventListener('click', function(event) {
+    var target = event.target;
 
-  if (target.className !== classNameCloseButton) {
-    return;
-  }
+    if (target.className !== classNameCloseButton) {
+      return;
+    }
 
-  var popup = event.target.closest('.popup');
+    var popup = event.target.closest('.popup');
 
-  popup.classList.remove(classNameOpenedPopup);
-});
+    popup.classList.remove(classNameOpenedPopup);
+  });
+})();
 
+
+(function () {
+  var productSlider = document.querySelector('.product-slider');
+  var activeSlider = productSlider.querySelector('.product-slider__item._active');
+
+  var classNameControl = 'js-product-control';
+
+  productSlider.addEventListener('click', function (event) {
+    var target = event.target;
+
+    if (!target.classList.contains(classNameControl)) {
+      return;
+    }
+
+    var sliderNumber = target.dataset.control;
+    var newActiveSlider = productSlider.querySelector('.product-slider__item[data-slider="' + sliderNumber + '"]')
+
+    if (activeSlider === newActiveSlider) {
+      return;
+    }
+
+    newActiveSlider.classList.add('_active');
+    activeSlider.classList.remove('_active');
+    activeSlider = newActiveSlider;
+  });
+})();
+
+(function () {
+  var serviceSlider = document.querySelector('.service-slider');
+  var activeSlider = serviceSlider.querySelector('.service-slider__item._active');
+
+  var classNameControl = 'js-service-control';
+
+  serviceSlider.addEventListener('click', function (event) {
+    var target = event.target;
+
+    if (!target.classList.contains(classNameControl)) {
+      return;
+    }
+
+    var sliderNumber = target.dataset.control;
+    var newActiveSlider = serviceSlider.querySelector('.service-slider__item[data-slider="' + sliderNumber + '"]')
+
+    if (activeSlider === newActiveSlider) {
+      return;
+    }
+
+    newActiveSlider.classList.add('_active');
+    activeSlider.classList.remove('_active');
+    activeSlider = newActiveSlider;
+  });
+})();
